ae88fd3038102ea102dd43589a9d218f30c523c6
kahono922 <jackson.kahono@student.moingaschool>
2022-10-08 16:42:57 +0300

    Final touches

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,29 @@ import { NavLink } from "react-router-dom";
 
 function Nav() {
       return (
-                  <div className="Nav">
+                  <div className="Nav"
+                  style={{
+                        display: "flex",
+                        justifyContent: "space-around",
+                        alignItems: "center",
+
+                  }}
+                  >
                         <div className="Nav-links">
                               <NavLink to="/poem-gram/" className={"Link"}>All poems</NavLink>
                               <NavLink to="/poem-gram/personal" className={"Link"}>Personal</NavLink>
                               <NavLink to="/poem-gram/liked" className={"Link"}>Liked</NavLink>
 
-                        <NavLink to="/poem-gram/create" className={"Btn"}>Create</NavLink>
+                        <NavLink to="/poem-gram/create" className={"Btn"}
+                        style={{
+                              padding: "10px",
+                              backgroundColor: "#0b489c",
+                              color: "white",
+                              borderRadius: "5px",
+                              textDecoration: "none",
+                              fontWeight: "bold",
+                        }}
+                        >Create</NavLink>
                         </div>
                   </div>
       );
